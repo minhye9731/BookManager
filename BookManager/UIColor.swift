049bd97732fe52671ceb036e316eb306ba986560
minhye9731 <jenney62639731@gmail.com>
2022-07-21 03:23:15 +0900

    어플 메인색상 셋팅

---
+++ BookManager/UIColor.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor.swift
+//  BookManager
+//
+//  Created by 강민혜 on 7/21/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let mainColorSet: [UIColor] = [
+        UIColor(named: "green1")!,
+        UIColor(named: "green2")!,
+        UIColor(named: "green3")!]
+}
