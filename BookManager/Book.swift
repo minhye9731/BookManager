e83b401b7a8ac389dd31e4231310af6405262f36
minhye9731 <jenney62639731@gmail.com>
2022-07-21 03:21:59 +0900

    Book 데이터 구조체 생성 및 데이터생성

---
+++ BookManager/Book.swift
@@ -0,0 +1,15 @@
+//
+//  Book.swift
+//  BookManager
+//
+//  Created by 강민혜 on 7/21/22.
+//
+
+import Foundation
+import UIKit
+
+struct Book {
+    var title: String
+    var rate: Double
+    var imageURL: String
+}
---
+++ BookManager/BookInfo.swift
@@ -0,0 +1,24 @@
+//
+//  BookInfo.swift
+//  BookManager
+//
+//  Created by 강민혜 on 7/21/22.
+//
+
+import Foundation
+
+struct BookInfo {
+    
+    let book: [Book] = [
+        Book(title: "역행자", rate: 9.5, imageURL: "https://image.yes24.com/goods/109705390/XL"),
+        Book(title: "작별인사", rate: 9.4, imageURL: "https://image.yes24.com/goods/108887930/XL"),
+        Book(title: "불편한 편의점", rate: 9.3, imageURL: "https://image.yes24.com/goods/99308021/XL"),
+        Book(title: "인플레이션에서 살아남기", rate: 9.7, imageURL: "https://image.yes24.com/goods/109323044/XL"),
+        Book(title: "기분을 관리하면 인생이 관리된다", rate: 9.5, imageURL: "https://image.yes24.com/goods/108624716/XL"),
+        Book(title: "설민석의 한국사 대모험 21", rate: 10.0, imageURL: "https://image.yes24.com/goods/109780053/XL"),
+        Book(title: "내가 틀릴 수도 있습니다", rate: 9.5, imageURL: "https://image.yes24.com/goods/108850617/XL"),
+        Book(title: "어서 오세요, 휴남동 서점입니다", rate: 9.2, imageURL: "https://image.yes24.com/goods/106211628/XL"),
+        Book(title: "세상의 마지막 기차역", rate: 9.6, imageURL: "https://image.yes24.com/goods/109308932/XL"),
+        Book(title: "주술회전 19 일반판", rate: 10.0, imageURL: "https://image.yes24.com/goods/110127724/XL")
+    ]
+}
