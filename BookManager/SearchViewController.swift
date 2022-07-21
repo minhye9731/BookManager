5fb75fd9ba8573e5c0df867a084be93b51cf01eb
minhye9731 <jenney62639731@gmail.com>
2022-07-21 18:30:30 +0900

    검색화면 identifier 설정 및 navigation item 셋팅

--- BookManager/SearchViewController.swift
+++ BookManager/SearchViewController.swift
@@ -8,13 +8,23 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    static var identifier = "SearchViewController"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        configureNav()
         
     }
     
-
+    func configureNav() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "xmark"), style: .plain, target: self, action: #selector(closeButtonTapped))
+    }
+    
+    @objc
+    func closeButtonTapped() {
+        self.dismiss(animated: true)
+    }
  
 }
